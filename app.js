806dc2ddc5a437fd4a26c1e19229e19c2b187f2a
Thomas <namulabre@gmail.com>
2024-05-06 20:24:20 +0200

    Swagger is not available in production environment

--- app.js
+++ app.js
@@ -14,47 +14,47 @@ const serverAddr = `http://${host}:${port}`;
 
 const env = process.env.NODE_ENV || 'development';
 
-if (env === 'development')
+if (env === 'development') {
     app.use(log);
-
-const options = {
-    definition: {
-        openapi: "3.1.0",
-        info: {
-            title: "AuthService",
-            version: "0.1.0",
-            description:
-                "AuthService is a micro service to manage user session of other application",
-            license: {
-                name: "MIT",
-                url: "https://spdx.org/licenses/MIT.html",
+    const options = {
+        definition: {
+            openapi: "3.1.0",
+            info: {
+                title: "AuthService",
+                version: "0.1.0",
+                description:
+                    "AuthService is a micro service to manage user session of other application",
+                license: {
+                    name: "MIT",
+                    url: "https://spdx.org/licenses/MIT.html",
+                },
             },
-        },
-        servers: [
-            {
-                url: serverAddr,
-            },
-        ],
-        components: {
-            securitySchemes: {
-                BearerAuth: {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT',
+            servers: [
+                {
+                    url: serverAddr,
+                },
+            ],
+            components: {
+                securitySchemes: {
+                    BearerAuth: {
+                        type: 'http',
+                        scheme: 'bearer',
+                        bearerFormat: 'JWT',
+                    },
                 },
             },
         },
-    },
-    apis: ["./routes/*.js"],
-};
+        apis: ["./routes/*.js"],
+    };
 
-const specs = swaggerJsdoc(options);
+    const specs = swaggerJsdoc(options);
 
-app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(specs, { explorer: true })
-);
+    app.use(
+        "/api-docs",
+        swaggerUi.serve,
+        swaggerUi.setup(specs, { explorer: true })
+    );
+}
 
 dotenv.config();
 app.use(bodyParser.json());
@@ -67,8 +67,6 @@ app.get("/", (req, res) => {
     res.json({message: 'Auth service online'});
 });
 
-
-
 app.listen(3000, () => {
-    console.log(`Server started on port 3000. Go to ${serverAddr}/api-docs/ to get documentation`);
+    console.log(`Server started on port 3000. Go to ${serverAddr}/api-docs/ to get documentation (only in development environment)`);
 });
