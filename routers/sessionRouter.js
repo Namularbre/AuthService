80263ca8f97d1f61d4669e4e1237dea27cc650dd
Thomas <namulabre@gmail.com>
2024-05-11 20:13:34 +0200

    Added a new route to logout, renamed create method to login

--- routers/sessionRouter.js
+++ routers/sessionRouter.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const SessionController = require('../controllers/sessionController');
+const login = require("../middlewares/login");
 
 const sessionRouter = express.Router();
 
 sessionRouter
     .route('/login')
-    .post(SessionController.create);
+    .post(SessionController.login);
 
 sessionRouter
     .route('/verify')
     .post(SessionController.verify);
+
+sessionRouter
+    .route('/logout')
+    .put(login, SessionController.logout);
 
 module.exports = sessionRouter;
