cf0f7a58a60d78a19956bb21de2cb6669969e460
Thomas <namulabre@gmail.com>
2024-05-14 19:12:36 +0200

    Added new table in DB to add groups

--- auth.sql
+++ auth.sql
@@ -24,3 +24,23 @@ create table sessions
     constraint sessions_users_FK
         foreign key (idUser) references users (idUser)
 );
+
+-- auth.groups definition
+
+CREATE TABLE `groups` (
+    `idGroup` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    PRIMARY KEY (`idGroup`),
+    UNIQUE KEY `groups_unique` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- auth.groups_users definition
+
+CREATE TABLE `groups_users` (
+    `idUser` int(11) NOT NULL,
+    `idGroup` int(11) NOT NULL,
+    UNIQUE KEY `groups_users_unique` (`idUser`,`idGroup`),
+    KEY `groups_users_groups_FK` (`idGroup`),
+    CONSTRAINT `groups_users_groups_FK` FOREIGN KEY (`idGroup`) REFERENCES `groups` (`idGroup`),
+    CONSTRAINT `groups_users_users_FK` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
